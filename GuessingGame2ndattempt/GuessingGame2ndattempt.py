d1f9d11ca3460dc59cc8bd8a6e98c884335212f4
Brooklyn Parr <paigebrooklyn22@gmail.com>
2023-01-14 18:31:52 -0800

    GuessingGame

--- GuessingGame2ndattempt/GuessingGame2ndattempt.py
+++ GuessingGame2ndattempt/GuessingGame2ndattempt.py
@@ -3,24 +3,23 @@ import random
 def display_title():
     print("Guess the number!")
     print()
-    print()
 
-def play_game(LIMIT):
-    number = random.randit(1, LIMIT)
+def play_game(LIMIT):    
+    number = random.randint(1, LIMIT)
     print(f"I'm thinking of a number from 1 to {LIMIT}\n")
     count = 1
     guess = int(input("Your guess: "))
 
     while (guess != number):
         if guess < number:
-            print ("Too low.")
+            print("Too low.")
             count += 1
         elif guess > number:
             print("Too high.")
             count += 1
         guess = int(input("Your guess: "))
     print(f"You guessed it in {count} tries.\n")
-
+     
 def main():
     display_title()
     again = "y"
@@ -31,7 +30,7 @@ def main():
         print()
     print("Bye!")
 
-    # if started as the main module, call the main function
-    if _name_ == "_main_":
-        main()
+# if started as the main module, call the main function
+if __name__ == "__main__":
+    main()
 
